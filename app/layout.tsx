be7b29e373e60a4b8c2b081e1bfcac9668a5e190
AndresLFDev <adlaraf1004@gmail.com>
2026-06-06 11:46:25 -0500

    feat: configure fonts and color variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 
-import { Fredoka } from "next/font/google"
+import { Fredoka, Nunito } from "next/font/google"
 
 const fredoka = Fredoka({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  variable: "--font-fredoka",
+})
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-nunito",
 })
 
 export const metadata: Metadata = {
@@ -21,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={fredoka.className}>
+      <body className={`${fredoka.variable} ${nunito.variable}`}>
         <Navbar />
         {children}
       </body>
